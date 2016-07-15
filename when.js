4cc5d7c35493700dc9a40f3cb6da6a2b6e3ce7dc
Matt McKegg <matt@wetsand.co.nz>
2016-07-15 16:42:04 +1200

    when: resolve inner observs if root is not observable

--- when.js
+++ when.js
@@ -1,11 +1,29 @@
 var computed = require('./computed')
+var isObservable = require('./is-observable')
 
 module.exports = When
 
 function When (obs, ifTrue, ifFalse) {
+  ifTrue = handleInnerValues(ifTrue)
+  ifFalse = handleInnerValues(ifFalse)
   return computed([obs, ifTrue, ifFalse], lambda)
 }
 
 function lambda (value, ifTrue, ifFalse) {
   return value ? ifTrue : ifFalse
+}
+
+function handleInnerValues (obs) {
+  if (Array.isArray(obs) && obs.some(isObservable)) {
+    // HACK: resolve inner observs
+    var inner = []
+    return computed(obs, function () {
+      for (var i = 0; i < arguments.length; i++) {
+        inner[i] = arguments[i]
+      }
+      inner.length = arguments.length
+      return inner
+    })
+  }
+  return obs
 }
