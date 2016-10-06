12f0318530920719cc13c2b7b7acc4484c6f4389
Matt McKegg <matt@wetsand.co.nz>
2016-10-06 17:24:48 +1300

    watch: allow no listener specified

--- watch.js
+++ watch.js
@@ -1,6 +1,7 @@
 module.exports = watch
 
 function watch (observable, listener) {
+  listener = listener || noop
   if (typeof observable === 'function') {
     var remove = observable(listener)
     listener(observable())
