beee53234fd649287c2d5ea0cf358350dead5ae7
Matt McKegg <matt@wetsand.co.nz>
2017-02-14 15:01:19 +1300

    add once-true helper

---
+++ once-true.js
@@ -0,0 +1,17 @@
+var watch = require('./watch')
+module.exports = function onceTrue (value, fn) {
+  var done = false
+  var release = watch(value, (v) => {
+    if (v && !done) {
+      done = true
+      setImmediate(doRelease)
+      fn(v)
+    }
+  }, { nextTick: true })
+
+  return release
+
+  function doRelease () {
+    release()
+  }
+}
